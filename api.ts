c595dcbf1bb76e7e98f4731119e628892f56afb3
unknown <sayamalvi07@gmail.com>
2026-08-07 16:26:40 +0530

    fix refresh endpoint not called

--- api.ts
+++ api.ts
@@ -5,24 +5,27 @@ export const api = axios.create({
     withCredentials: true
 });
 
-api.interceptors.request.use((config) => {
-    const token = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error)
-});
-
 api.interceptors.response.use(
     (response) => response,
-    (error => {
-        if (error.response?.status === 401) {
-            if (typeof window !== 'undefined') {
-                localStorage.removeItem('accessToken')
+    async (error) => {
+        const originalRequest = error.config
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            if (originalRequest.url === '/auth/refresh') {
+                window.location.href = '/login'
+                return Promise.reject()
             }
-            window.location.href = '/login'
+            originalRequest._retry = true;
+            try {
+                await api.post('/auth/refresh')
+                return api(originalRequest
+                )
+            }
+            catch (refreshError) {
+                window.location.href = '/login'
+                return Promise.reject(refreshError)
+            }
         }
         return Promise.reject(error)
-    }))
+    }
+)
